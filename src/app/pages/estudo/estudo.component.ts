a496e7df6f77c418c5cff6fde8179f12ac0d6f13
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-09-30 17:12:18 -0300

    desenvolvimento completo um site simples

--- src/app/pages/estudo/estudo.component.ts
+++ src/app/pages/estudo/estudo.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-estudo',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './estudo.component.html',
   styleUrl: './estudo.component.css'
 })
 export class EstudoComponent {
+  title = 'Estudar sempre faz a diferença'
+  devestudo = 'https://www.udc.edu.br/udcead/assets/images/courses/pic7.jpg'
+  altDevestudo = 'desenvolvimento de estudo'
+  desenvolverurl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD_P5M918UO29poLJ3UuyRH5G76J2tIWBpeg&s'
+  devAlt = 'desenvolvimento'
 
 }
--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.css'
 })
 export class SobreComponent {
 
+  urlimagem = 'https://canaldoprodutor.tv.br/wp-content/uploads/2022/05/o-que-faz-um-analista-de-sistemas.jpg'
+  devAlt = 'estudo de desenvolvimento de sistemas'
 }
