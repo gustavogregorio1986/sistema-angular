7c3797cfba3bc7d077b00b57ee79490439e7c15b
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-09-30 16:04:11 -0300

    desenvolvimwento de frontend com menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  imagemurl = 'http://arquivo.devmedia.com.br/marketing/img/artigo-atividades-basicas-ao-processo-de-desenvolvimento-de-software-5413.png'
+  altImagem = "desenvolvendo"
 }
